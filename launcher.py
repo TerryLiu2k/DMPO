45a30195660a8b5bcdef2466419babd943e6dd1f
Henry Bi <HenryB@gmail.com>
2021-05-26 04:21:57 +0800

    added config for conservative multiagent MBPO and p&pi raidus=2 on CACC removed the confusing arg "branch" for MBPO

--- launcher.py
+++ launcher.py
@@ -1,4 +1,6 @@
-from algorithms.config.CACC_MBPO import main
+import os
+from algorithms.config.CACC_MBPO_conservative import main
 from algorithms.envs.CACC import CACC_catchup, CACC_slowdown
-init_checkpoint = "checkpoints/CACC_catchup_MultiagentMBPO_20905/12001_-247.26344310442602.pt"
-main(env_fn=CACC_slowdown, init_checkpoint=None, debug=False, test=False, seed=None, device=0)
+os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+#init_checkpoint = "checkpoints/CACC_catchup_MultiagentMBPO_20905/12001_-247.26344310442602.pt"
+main(env_fn=CACC_catchup, init_checkpoint=None, debug=False, test=False, seed=None, device=0)
